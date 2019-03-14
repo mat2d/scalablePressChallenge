064df50eb0dba9f62712fdaa48f3002375ed8056
Martin Arroyo <mat2dd@gmail.com>
2019-03-14 09:20:16 -0600

    Limpieza CoinFlip

--- CoinFlip/coinFlip.js
+++ CoinFlip/coinFlip.js
@@ -1,4 +1,3 @@
-clear();
 function flip(n){
     if (n <= 0 || n > 1000000){
     throw "ERROR - n value not allowed";
@@ -9,10 +8,9 @@ function flip(n){
         omega = false;
         number_length = n.toString().length;
         unix_date = Date.now().toString().split("").reverse().toString().replace(/,/g, '');
-        //theta = unix_date.toString().length;
         pointer = 0;
         while(omega == false){ 
-            random_number = unix_date.substring(pointer, pointer+ number_length);
+            random_number = unix_date.substring(pointer, pointer + number_length);
             if(parseInt(random_number) < n){
                 omega = true;
             }
